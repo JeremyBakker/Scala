2aa97599dc26cb44e7c588a320542f16d6e21584
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-07-18 13:05:45 -0500

    more basic pyspark

--- pyspark.py
+++ pyspark.py
@@ -17,7 +17,35 @@ pythonLines.persist
 # Parallelize data to create an RDD
 lines = sc.parallelize(["pandas", "i like pandas"])
 
+# Create an RDD of a log file and filter on "ERROR" and "WARNING"
 inputRDD = sc.textFile("log.txt")
 errorsRDD = inputRDD.filter(lambda x: "ERROR" in x)
 warningsRDD = inputRDD.filter(lambda x: "WARNING" in x)
 badLinesRDD = errorsRDD.union(warningsRDD)
+# Print results
+print("Input had " + str(badLinesRDD.count()) + " concerning lines")
+print("Here are 10 examples:")
+for line in badLinesRDDtake(10):
+    print(line)
+
+# Filter by passing in a function
+def containsError(line):
+    return "ERROR" in line
+word = inputRDD.filter(containsError)
+
+# Basic map function
+nums = sc.parallelize([1, 2, 3, 4])
+squared = nums.map(lambda x: x * x).collect() 
+for num in squared:
+    print ("%i " % (num))
+
+# Basic flat map function
+lines = sc.parallelize(["hello world", "hi"]) 
+words = lines.flatMap(lambda line: line.split(" ")) 
+words.first()
+
+nums_two = sc.parallelize([3,4,5,6])
+
+nums.union(nums_two)
+nums.intersection(nums_two)
+nums.cartesian(nums_two).collect()
