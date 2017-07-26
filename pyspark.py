645e7e3bf6a5dc97d77e7fc851f346c0fff73444
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-07-26 16:57:52 -0500

    beginning joins

--- pyspark.py
+++ pyspark.py
@@ -254,3 +254,28 @@ df.select(sumDistinct("Quantity")).show()
 
 from pyspark.sql.functions import sum, count, avg, expr
 df.select(count("Quantity").alias("total_transactions"),sum("Quantity").alias("total_purchases"),avg("Quantity").alias("avg_purchases"),expr("mean(Quantity)").alias("mean_purchases")).selectExpr("total_purchases/total_transactions","avg_purchases","mean_purchases").collect()
+
+from pyspark.sql.functions import var_pop, stddev_pop, var_samp, stddev_samp
+df.select(var_pop("Quantity"),var_samp("Quantity"),stddev_pop("Quantity"),stddev_samp("Quantity")).collect()
+
+df.groupBy("Product","Product type").count().show()
+
+df.groupBy("Product").agg(count("Quantity").alias("quan"),expr("count(Quantity)")).show()
+
+df.groupBy("Product").agg(expr("avg(Quantity)"),expr("stddev_pop(Quantity)")).show()
+
+
+# Joins
+person = spark.createDataFrame([(0, "Bill Chambers", 0, [100]), (1, "Matei Zaharia", 1, [500, 250, 100]), (2, "Michael Armbrust", 1, [250, 100])]).toDF("id", "name", "graduate_program", "spark_status")
+graduateProgram = spark.createDataFrame([(0,"Masters", "School of Information", "UC Berkeley"),(2, "Masters", "EECS", "UC Berkeley"),(1, "Ph.D.", "EECS", "UC Berkeley")]).toDF("id", "degree","department","school")
+sparkStatus = spark.createDataFrame([(500, "Vice President"),(250, "PMC Member"),(100,"Contributor")]).toDF("id","status")
+person.createOrReplaceTempView("person")
+graduateProgram.createOrReplaceTempView("graduateProgram")
+sparkStatus.createOrReplaceTempView("sparkStatus")
+
+# Inner Joins
+joinExpression = person.graduate_program == graduateProgram.id
+person.join(graduateProgram, joinExpression).show()
+person.join(graduateProgram, joinExpression, "inner").show()
+person.join(graduateProgram, joinExpression, "outer").show()
+person.join(graduateProgram, joinExpression, "left_outer").show()
